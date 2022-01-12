4b42a18eb05b8c4a5fb2d74a11a76ddb911e5fcd
gtrman97 <andrew.killian@g.austincc.edu>
2022-01-12 02:43:35 -0600

    made changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
 
 const users = [];
 
@@ -7,8 +8,14 @@ app.get('/users', (req, res) => {
     res.json(users);
 });
 
-app.post('/users', (req, rest) => {
-    
+app.post('/users', async (req, rest) => {
+    try {
+        const salt = await bcrypt.genSalt();
+    }
+    const user = {user: req.body.name, password: req.body.password};
+    users.push(user);
+    res.status(201).send();
+
 })
 
 app.listen(3000);
