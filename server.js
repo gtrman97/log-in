e8b3b7df38c9da21caee3412d2dfe0352a12eafd
gtrman97 <andrew.killian@g.austincc.edu>
2022-01-12 02:36:05 -0600

    added request file

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const app = express();
 
+const users = [];
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+app.post('/users', (req, rest) => {
+    
+})
+
 app.listen(3000);
